587d449b017c006211ed9e9fe969dba6c520ca5f
Carol Malveira <carolmalveiraa12@icloud.com>
2024-11-07 22:02:16 -0300

    adiciona componentes de agendamento, finalizar, home, login e pagamento com testes

---
+++ src/app/finalizar/finalizar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-finalizar',
+  standalone: true,
+  imports: [],
+  templateUrl: './finalizar.component.html',
+  styleUrl: './finalizar.component.scss'
+})
+export class FinalizarComponent {
+
+}
